9a82ec6b5f04e1f71b347a4fda125f92606e55cc
Petar Stoenchev <peter.stoenchev@abv.bg>
2021-01-31 16:06:46 +0200

    Initial Commit

---
+++ CarInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarInfo
+//
+//  Created by Petar Stoenchev on 31.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
