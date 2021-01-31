18cea6c72f238c8ab8ef38264b878ceb3b5c322a
Petar Stoenchev <peter.stoenchev@abv.bg>
2021-02-01 01:24:57 +0200

    First Screen is ready

---
+++ CarInfo/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  CarInfo
+//
+//  Created by Petar Stoenchev on 31.01.21.
+//
+
+import UIKit
+
+
+// MARK: - Class Definition
+
+final class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    @IBAction func closebutton(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+}
--- CarInfo/ViewController.swift
+++ CarInfo/ViewController.swift
@@ -7,10 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+// MARK: - Class Definition
+
+final class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet private weak var carSearchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        carSearchBar.setImage(UIImage(named: "icon"), for: .search, state: .normal)
+        carSearchBar.setImage(UIImage(named: "camicon"), for: .bookmark, state: .normal)
         // Do any additional setup after loading the view.
     }
 
