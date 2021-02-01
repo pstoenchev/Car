1d8741c42aae1eb8771959cbc3aebb4ff83bf6db
Petar Stoenchev <peter.stoenchev@abv.bg>
2021-02-01 16:42:33 +0200

    add secondViewController

--- CarInfo/SecondViewController.swift
+++ CarInfo/SecondViewController.swift
@@ -11,17 +11,29 @@ import UIKit
 // MARK: - Class Definition
 
 final class SecondViewController: UIViewController {
-
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
+        downloadPhoto()
     }
     
 
-    @IBAction func closebutton(_ sender: UIButton) {
+    @IBAction private func closebutton(_ sender: UIButton) {
         dismiss(animated: true)
     }
+}
+
+// MARK: - Actions
+
+extension SecondViewController {
     
+    func downloadPhoto(){
+        guard let url = URL(string: "https://www.jimbars.co.uk/wp-content/uploads/2020/01/vw-logo-1.png") else { return }
+        
+        if let data = try? Data(contentsOf: url) {
+            imageView.image = UIImage(data: data)
+        }
+    }
 }
--- CarInfo/ViewController.swift
+++ CarInfo/ViewController.swift
@@ -10,17 +10,14 @@ import UIKit
 
 // MARK: - Class Definition
 
-final class ViewController: UIViewController, UISearchBarDelegate {
+final class ViewController: UIViewController {
     
     @IBOutlet private weak var carSearchBar: UISearchBar!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         carSearchBar.setImage(UIImage(named: "icon"), for: .search, state: .normal)
         carSearchBar.setImage(UIImage(named: "camicon"), for: .bookmark, state: .normal)
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
